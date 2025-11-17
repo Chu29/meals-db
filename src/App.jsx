874b1edc49de5fbf1c0bd859a515a58a9041113c
Chu A. <chu.amk22@gmail.com>
2025-11-17 11:02:12 +0100

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage/HomePage";
+import MealsPage from "./pages/MealsPage";
+import MealDetailsPage from "./pages/MealDetailsPage";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage}></Route>
+        <Route path="/meal/category/:category" Component={MealsPage}></Route>
+        <Route path="/meal/:id" Component={MealDetailsPage}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
