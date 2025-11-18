8262ecc752ab3f7a1672155e35677afbb4803867
Chu A. <chu.amk22@gmail.com>
2025-11-18 11:58:46 +0100

    feat(ui): Implement application HomePage with each meal category

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import "./Header.css";
+import { useQuery } from "@tanstack/react-query";
+import { searchMealsByName } from "../../services/meal.service";
+
 function SearchBar() {
+  const [mealName, setMealName] = useState();
+
+  const { data } = useQuery({
+    queryKey: ["search", mealName],
+    queryFn: () => searchMealsByName(mealName),
+  });
+
+  console.log("Search Data", data);
+
   return (
-    <form className="search-bar" onSubmit={(event) => event.preventDefault()}>
-      <input type="text" placeholder="Search for a meal" />
+    <form className="search-bar" onSubmit={(event) => event.preventDefault}>
+      <input
+        type="text"
+        placeholder="Search for a meal..."
+        value={mealName}
+        onChange={(event) => setMealName(event.target.value)}
+      />
     </form>
   );
 }
 
 export default function Header() {
   return (
-    <header className="header">
-      <SearchBar />
-    </header>
+    <>
+      <header className="header">
+        <SearchBar />
+      </header>
+    </>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,17 @@
+import MealCategoryCard from "../../components/MealCategoryCard";
+import Header from "../../components/Header/Header";
+import "./HomePage.css";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <div className="header">
+        <h1>MealDB.</h1>
+        <Header />
+      </div>
+      <main className=".category-cards-container">
+        <MealCategoryCard />
+      </main>
+    </>
+  );
 }
