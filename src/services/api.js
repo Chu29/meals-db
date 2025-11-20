956b8310151e89d9f99ae5ed5b4312fbf984285e
Chu A. <chu.amk22@gmail.com>
2025-11-20 14:35:46 +0100

    refactor: Create utils folder to store constants

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import ky from "ky";
-
-const API_BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+import { API_BASE_URL } from "../utils/constants";
 
 export const api = ky.create({
   prefixUrl: API_BASE_URL,
