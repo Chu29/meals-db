8dd6489192e0c2f364becd7b2f8ae1a4c49af186
Chu A. <chu.amk22@gmail.com>
2025-11-20 14:29:05 +0100

    refactor: add new route for SearchedMealPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage/HomePage";
 import MealsPage from "./pages/MealsPage";
 import MealDetailsPage from "./pages/MealDetailsPage";
+import SearchedMealsPage from "./pages/SearchedMealsPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" Component={HomePage}></Route>
         <Route path="/meal/category/:category" Component={MealsPage}></Route>
         <Route path="/meal/:id" Component={MealDetailsPage}></Route>
+        <Route path="/meal/search/:name" Component={SearchedMealsPage}></Route>
       </Routes>
     </BrowserRouter>
   );
